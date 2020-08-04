2f252d1f88565589a9577b0aae6b25877ad25b24
urgyen <imalyanov@gmail.com>
2020-08-04 14:56:20 -0500

    refactor: refactor reducer
    promisify fs methods

--- src/reducers.js
+++ src/reducers.js
@@ -5,40 +5,21 @@ import fs from "fs"
 const toPromise = f => a => (
   new Promise((res, rej) => f(a, (err, fa) => err ? rej(err) : res(fa))))
 
-const readDir = path => new Promise((res, rej) => {
-  fs.readdir(path, (err, content) => {
-    if (err) {
-      rej(err)
-    } else {
-      res({content})
-    }
-  })
-})
-
-const readFile = path => new Promise((res, rej) => {
-  fs.readFile(path, {encoding: "utf8"}, (err, content) => {
-    if (err) {
-      rej(err)
-    } else {
-      res({content})
-    }
-  })
-})
-
+const readDir = toPromise(fs.readdir)
+const readFile = toPromise((path, g) => fs.readFile(path, {encoding: "utf8"}, g))
 const getStats = toPromise(fs.stat)
 
 export async function getContents(path) {
   const stats = await getStats(path)
   const content = stats.isDirectory() ? await readDir(path) : await readFile(path)
 
-  return content
+  return {content}
 }
 
 export async function getPath({path, dir}) {
   const [childPath, parentPath] = await Promise.all([
     `${path}/${dir[0]}`,
-    new Promise((res, rej) => fs.realpath(path, (err, resolvedPath) => (
-      err ? rej(err) : res(resolvedPath.split("/").slice(0, -1).join("/"))))),
+    toPromise(fs.realpath)(path).then(p => p.split("/").slice(0, -1).join("/")),
   ])
 
   return {
@@ -56,11 +37,14 @@ export const getPathFailure = createAction("GET_PATH_FAILURE")
 
 const initialState = {}
 const reducer = createReducer(initialState, {
-  INIT: (s, {payload: {path}}) => loop({...s, currentPath: path}, Cmd.run(getContents, {
-    successActionCreator: initSuccess,
-    failActionCreator: initFailure,
-    args: [path],
-  })),
+  INIT: (s, {payload: {path}}) => loop(
+    {...s, currentPath: path},
+    Cmd.run(getContents, {
+      successActionCreator: initSuccess,
+      failActionCreator: initFailure,
+      args: [path],
+    }),
+  ),
   INIT_SUCCESS: (s, {payload: {content}}) => loop(
     {...s, currentDir: content},
     Cmd.run(getPath, {
