1413a54bfa557668b16fef4ab905285142b98d6d
urgyen <imalyanov@gmail.com>
2020-08-04 20:47:19 -0500

    test: add mocks for fs

--- src/mocks.js
+++ src/mocks.js
@@ -4,18 +4,21 @@ export default {
   restore: mock.restore,
   mockFs: () => mock({
     "/mock/dir1": {
-      "file.empty": "",
-      "file.txt": "text file content",
-      "file.bin": Buffer.from([8, 6, 7, 5, 3, 0, 9]),
+      "empty-file.txt": "",
+      "text-file.txt": "text file content",
+      "file.jpg": Buffer.from([8, 6, 7, 5, 3, 0, 9]),
       "empty-dir": {},
       dir: {
         file1: "text file 1",
-        file2: Buffer.from([3, 0, 9, 8, 6, 7, 5]),
+        file2: "",
       },
     },
     "/mock/denied": mock.directory({
       mode: 0o000,
       items: [],
+    }),
+    "/mock/file-denied.txt": mock.file({
+      mode: 0o000,
     }),
   }),
 }
