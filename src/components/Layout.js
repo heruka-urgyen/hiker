c592422e131f2cc27dbe54a0b8624870bfb707a3
urgyen <imalyanov@gmail.com>
2020-08-05 13:09:42 -0500

    feat: add layout component
    that describes basic layout with two, three or more panes and reacts on
    key events to switch between panes

---
+++ src/components/Layout.js
@@ -0,0 +1,41 @@
+/* eslint-disable react/jsx-props-no-spreading */
+
+import React from "react"
+import {Box, useInput} from "ink"
+import PropTypes from "prop-types"
+
+const Pane = React.forwardRef(({children, ...props}, ref) => (
+  <Box flexGrow={1} ref={ref} {...props}>
+    {children}
+  </Box>
+))
+
+const Layout = ({children, onMoveLeft, onMoveRight, ...props}) => {
+  useInput((input, _) => {
+    if (input === "l") {
+      onMoveRight()
+    }
+
+    if (input === "h") {
+      onMoveLeft()
+    }
+  })
+
+  return (
+    <Box flexDirection="row" {...props}>
+      {children}
+    </Box>
+  )
+}
+
+Pane.propTypes = {
+  children: PropTypes.element.isRequired,
+}
+
+Layout.propTypes = {
+  children: PropTypes.element.isRequired,
+  onMoveLeft: PropTypes.func.isRequired,
+  onMoveRight: PropTypes.func.isRequired,
+}
+
+export default {Layout, Pane}
