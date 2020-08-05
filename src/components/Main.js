34c17a9e51603c0b98e77c1784d115eac27ea017
urgyen <imalyanov@gmail.com>
2020-08-05 14:03:58 -0500

    feat: add main component
    that declares how application is rendered

---
+++ src/components/Main.js
@@ -0,0 +1,131 @@
+import React, {useState, useEffect, useRef} from "react"
+import PropTypes from "prop-types"
+import {useSelector, useDispatch} from "react-redux"
+import {Text, measureElement} from "ink"
+import useStdoutDimensions from "ink-use-stdout-dimensions"
+import SelectInput from "ink-select-input"
+
+import {Layout, Pane} from "./Layout"
+import {selectItem, goBack, goForward} from "../reducers"
+
+const Renderer = props => {
+  const {
+    data,
+    limit,
+    w,
+    onHighlight,
+    isFocused,
+    initialIndex,
+  } = props
+
+  if (Array.isArray(data)) {
+    return (
+      <SelectInput
+        limit={limit}
+        initialIndex={initialIndex}
+        isFocused={isFocused}
+        items={data}
+        onHighlight={onHighlight}
+        indicatorComponent={Text}
+        itemComponent={({label, isSelected}) => (
+          <Text
+            backgroundColor={isSelected ? "red" : "none"}
+            wrap="truncate"
+          >
+            {` ${label}${Array(Math.max(0, w - label.length - 2)).fill(" ").join("")}`}
+          </Text>
+        )}
+      />
+    )
+  }
+
+  return <Text key={1} wrap="truncate">{data}</Text>
+}
+
+const Main = () => {
+  const [r1, r2, r3] = [useRef(), useRef(), useRef()]
+  const [[w1, w2, w3], setWidth] = useState([0, 0, 0])
+  const [_, rows] = useStdoutDimensions()
+  const dispatch = useDispatch()
+  const {
+    currentContent,
+    currentSelected,
+    currentPath,
+    childContent,
+    childSelected,
+    childPath,
+    parentContent,
+    parentSelected,
+    parentPath,
+  } = useSelector(s => s)
+
+  useEffect(() => {
+    setWidth([r1, r2, r3].map(r => measureElement(r.current).width))
+  }, [])
+
+  const onMoveRight = () => {
+    dispatch(goForward())
+  }
+
+  const onMoveLeft = () => {
+    dispatch(goBack())
+  }
+
+  const onHighlight = item => {
+    dispatch(selectItem({currentSelected: item}))
+  }
+
+  const mapModel = (data, path) => data.map(
+    label => ({label, path, value: `${path}/${label}`}),
+  )
+
+  return (
+    <Layout height={10} onMoveLeft={onMoveLeft} onMoveRight={onMoveRight}>
+      <Pane ref={r1} key={0} width="10%">
+        <Renderer
+          limit={rows}
+          initialIndex={parentSelected}
+          data={mapModel(parentContent, parentPath)}
+          w={w1}
+        />
+      </Pane>
+      <Pane ref={r2} key={1} width="40%">
+        <Renderer
+          isFocused
+          limit={rows}
+          initialIndex={currentSelected}
+          onHighlight={onHighlight}
+          data={mapModel(currentContent, currentPath)}
+          w={w2}
+        />
+      </Pane>
+      <Pane ref={r3} key={2} width="50%">
+        <Renderer
+          limit={rows}
+          initialIndex={childSelected}
+          data={mapModel(childContent, childPath)}
+          w={w3}
+        />
+      </Pane>
+    </Layout>
+  )
+}
+
+Renderer.defaultProps = {
+  w: 0,
+  isFocused: false,
+  initialIndex: 0,
+  onHighlight: _ => _,
+  limit: 20,
+}
+
+Renderer.propTypes = {
+  w: PropTypes.number,
+  data: PropTypes.arrayOf(PropTypes.oneOfType([PropTypes.object])).isRequired,
+  limit: PropTypes.number,
+  isFocused: PropTypes.bool,
+  onHighlight: PropTypes.func,
+  initialIndex: PropTypes.number,
+}
+
+export default Main
--- src/index.js
+++ src/index.js
@@ -1,5 +1,11 @@
+import React from "react"
+import {Provider} from "react-redux"
+import {render} from "ink"
 import process from "process"
 import store from "./store"
 import {init} from "./reducers"
+import Main from "./components/Main"
 
 store.dispatch(init({path: process.cwd()}))
+
+render(<Provider store={store}><Main /></Provider>)
